6c42cc0a2e94ceeeb1dae8baa30791d181b2abe0
biggycao <116410740+biggycao@users.noreply.github.com>
2023-12-07 06:55:07 +0800

    Update pm.sh

--- pm.sh
+++ pm.sh
@@ -2,7 +2,9 @@ packagesNeeded='curl jq'
 if [ -x "$(command -v apk)" ];       then sudo apk add --no-cache $packagesNeeded
 elif [ -x "$(command -v apt-get)" ]; then sudo apt-get install $packagesNeeded
 elif [ -x "$(command -v dnf)" ];     then sudo dnf install $packagesNeeded
-elif [ -x "$(command -v pacman)" ];     then sudo pacman -S $packagesNeeded
-elif [ -x "$(command -v emerge)" ];     then emerge -avjp $packagesNeeded
+elif [ -x "$(command -v pacman)" ];  then sudo pacman -S $packagesNeeded
+elif [ -x "$(command -v emerge)" ];  then emerge -avjp $packagesNeeded
+elif [ -x "$(command -v yum)" ];     then sudo yum install $packagesNeeded
 elif [ -x "$(command -v zypper)" ];  then sudo zypper install $packagesNeeded
+elif [ -x "$(command -v apt)" ]; then sudo apt-get install $packagesNeeded
 else echo "FAILED TO INSTALL PACKAGE: Package manager not found. You must manually install: $packagesNeeded">&2; fi
