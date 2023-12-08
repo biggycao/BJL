5befd8876d9a8b22d27baed084c80f49c612c2a0
biggycao <116410740+biggycao@users.noreply.github.com>
2023-12-08 18:40:52 +0800

    edit main.sh

---
+++ main.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if mount | grep -q 'devpts on /dev/pts' && [ -e /dev/ptmx ]
+then echo "OK:    Linux Kernel supports UNIX 98 PTY";
+else echo "ERROR: Linux Kernel does NOT support UNIX 98 PTY"; exit 2
+
+bash ./check.sh
