595e9a52abc8547ba8b15b1f650fdb4ac2c4834d
biggycao <116410740+biggycao@users.noreply.github.com>
2023-12-08 19:32:00 +0800

    update partition program

--- part.sh
+++ part.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 lsblk -f
 read -r -p "which disk do you want to install BJL on? (example /dev/sda) " disk
-parted --script $disk mklabel gpt
+echo "Partition Manually. God knows what you're doin' w/ the partitions."
+# some users wanna seperate /home and /usr partition. And I'm too lazy to sort all of these things out.
+fdisk $disk 
+
+sh arch
