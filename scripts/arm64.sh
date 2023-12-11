861d0ac58d01512d9b2eff08844aaecee6ddd7b8
biggycao <116410740+biggycao@users.noreply.github.com>
2023-12-11 19:34:01 +0800

    Update arm64.sh

---
+++ scripts/arm64.sh
@@ -0,0 +1,101 @@
+echo "COMPILE CROSS-TOOLCHAIN-BINUTILS"
+cd /mnt/lfs/sources
+tar -xf binutils-2.41.tar.xz
+cd binutils-2.41
+mkdir -v build
+cd build
+../configure --prefix=/mnt/lfs/tools \
+             --with-sysroot=/mnt/lfs \
+             --target=$LFS_TGT   \
+             --disable-nls       \
+             --enable-gprofng=no \
+             --disable-werror
+make
+make install
+cd /mnt/lfs/sources
+rm -r binutils-2.41
+echo "COMPILE CROSS-TOOLCHAIN-BINUTILS"
+tar -xf gcc-13.2.0.tar.xz
+cd gcc-13.2.0
+tar -xf ../mpfr-4.2.1.tar.xz
+mv -v mpfr-4.2.1 mpfr
+tar -xf ../gmp-6.3.0.tar.xz
+mv -v gmp-6.3.0 gmp
+tar -xf ../mpc-1.3.1.tar.gz
+mv -v mpc-1.3.1 mpc
+sed -e '/lp64=/s/lib64/lib/' \
+    -i.orig gcc/config/aarch64/t-aarch64-linux
+mkdir -v build
+cd       build
+../configure                  \
+    --target=$LFS_TGT         \
+    --prefix=/mnt/lfs/tools   \
+    --with-glibc-version=2.38 \
+    --with-sysroot=/mnt/lfs   \
+    --with-newlib             \
+    --without-headers         \
+    --enable-default-pie      \
+    --enable-default-ssp      \
+    --disable-nls             \
+    --disable-shared          \
+    --disable-multilib        \
+    --disable-threads         \
+    --disable-libatomic       \
+    --disable-libgomp         \
+    --disable-libquadmath     \
+    --disable-libssp          \
+    --disable-libvtv          \
+    --disable-libstdcxx       \
+    --enable-languages=c,c++
+make
+make install
+cd ..
+cat gcc/limitx.h gcc/glimits.h gcc/limity.h > \
+  `dirname $($LFS_TGT-gcc -print-libgcc-file-name)`/include/limits.h
+cd /mnt/lfs/sources
+rm -r gcc-13.2.0
+tar -xf linux-6.6.3.tar.xz
+make mrproper
+make headers
+find usr/include -type f ! -name '*.h' -delete
+cp -rv usr/include $LFS/usr
+cd /mnt/lfs/sources
+rm -r linux-6.6.3
+tar -xf glibc-2.38.tar.xz
+cd glibc-2.38
+patch -Np1 -i ../glibc-2.38-fhs-1.patch
+mkdir -v build
+cd       build
+echo "rootsbindir=/usr/sbin" > configparms
+../configure                             \
+      --prefix=/usr                      \
+      --host=$LFS_TGT                    \
+      --build=$(../scripts/config.guess) \
+      --enable-kernel=4.14               \
+      --with-headers=/mnt/lfs/usr/include    \
+      --disable-nscd                     \
+      libc_cv_slibdir=/usr/lib
+make
+make DESTDIR=/mnt/lfs install
+sed '/RTLDLIST=/s@/usr@@g' -i $LFS/usr/bin/ldd
+echo 'int main(){}' | $LFS_TGT-gcc -xc -
+readelf -l a.out | grep ld-linux
+echo "EGOUTPUT:[Requesting program interpreter: /lib/ld-linux-aarch64.so.1]"
+read comfirm
+rm -v a.out
+cd /mnt/lfs/sources
+tar -xf gcc-13.2.0.tar.xz
+cd gcc-13.2.0
+mkdir -v build
+cd       build
+../libstdc++-v3/configure           \
+    --host=$LFS_TGT                 \
+    --build=$(../config.guess)      \
+    --prefix=/usr                   \
+    --disable-multilib              \
+    --disable-nls                   \
+    --disable-libstdcxx-pch         \
+    --with-gxx-include-dir=/tools/$LFS_TGT/include/c++/13.2.0
+make
+make DESTDIR=/mnt/lfs install
+rm -v /mnt/lfs/usr/lib/lib{stdc++{,exp,fs},supc++}.la
