4242b5d8673bf3fcc20d79a1491e2d145503325d
biggycao <116410740+biggycao@users.noreply.github.com>
2023-12-11 19:33:54 +0800

    Update x86.sh

--- scripts/x86.sh
+++ scripts/x86.sh
@@ -19,15 +19,4 @@ case $(uname -m) in
 esac
 mkdir -pv $LFS/tools
 
-echo "4.3. Adding the LFS User"
-groupadd lfs
-useradd -s /bin/bash -g lfs -m -k /dev/null lfs
-echo ENTER YOUR LFS USER PASSWORD
-passwd lfs
-chown -v lfs $LFS/{usr{,/*},lib,var,etc,bin,sbin,tools}
-case $(uname -m) in
-  x86_64) chown -v lfs $LFS/lib64 ;;
-esac
-cat << EOF | su - lfs
-ls /
-EOF
+
