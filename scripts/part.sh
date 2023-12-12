4530b8b9bc6e55dca350d183ee7a76ebb0785215
biggycao <116410740+biggycao@users.noreply.github.com>
2023-12-12 12:16:34 +0800

    Update part.sh

--- scripts/part.sh
+++ scripts/part.sh
@@ -8,17 +8,19 @@ EFI_SIZE="1G"           # Size of EFI partition
 SWAP_SIZE="6G"          # Size of Swap partition
 
 # Partition the disk
-sudo parted $disk mklabel gpt \
+parted $disk mklabel gpt \
   mkpart primary fat32 1MiB $EFI_SIZE \
   set 1 esp on \
   mkpart primary linux-swap $EFI_SIZE $(($EFI_SIZE + $(echo $SWAP_SIZE | sed 's/G//') * 1024)) \
   mkpart primary ext4 $(($EFI_SIZE + $(echo $SWAP_SIZE | sed 's/G//') * 1024 + 1)) 100%
 
 # Format partitions
-sudo mkfs.fat -F32 ${disk}1
-sudo mkswap ${disk}2
-sudo mkfs.ext4 ${disk}3
+mkfs.fat -F32 ${disk}1
+mkswap ${disk}2
+mkfs.ext4 ${disk}3
 
 # Display partition information
-sudo parted $disk print
+parted $disk print
+mount $disk /mnt/lfs
+swapon $disk
 sh arch
