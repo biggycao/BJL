3e844e95945e2f9cbe303aeb4ca6315f218016a7
biggycao <116410740+biggycao@users.noreply.github.com>
2023-12-15 18:18:58 +0800

    optimize checking & bugfix

--- scripts/check.sh
+++ scripts/check.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 # A script to list version numbers of critical development tools
+
 # If you have tools installed in other directories, adjust PATH here AND
 # in ~lfs/.bashrc (section 4.4) as well.
+
 LC_ALL=C 
 PATH=/usr/bin:/bin
+
 bail() { echo "FATAL: $1"; exit 1; }
 grep --version > /dev/null 2> /dev/null || bail "grep does not work"
 sed '' /dev/null || bail "sed does not work"
 sort   /dev/null || bail "sort does not work"
+
 ver_check()
 {
    if ! type -p $2 &>/dev/null
    then 
-     echo "$2"; return 1; 
+     echo "ERROR: Cannot find $2 ($1)"; return 1; 
    fi
    v=$($2 --version 2>&1 | grep -E -o '[0-9]+\.[0-9\.]+[a-z]*' | head -n1)
    if printf '%s\n' $3 $v | sort --version-sort --check &>/dev/null
    then 
-     printf ""; return 0;
+     printf "OK:    %-9s %-6s >= $3\n" "$1" "$v"; return 0;
    else 
-     printf "%-9s" "$1"; 
+     printf "ERROR: %-9s is TOO OLD ($3 or later required)\n" "$1"; 
      return 1; 
    fi
 }
@@ -29,15 +33,15 @@ ver_kernel()
    kver=$(uname -r | grep -E -o '^[0-9\.]+')
    if printf '%s\n' $1 $kver | sort --version-sort --check &>/dev/null
    then 
-     printf "$kver >= $1\n"; return 0;
+     printf "OK:    Linux Kernel $kver >= $1\n"; return 0;
    else 
-     printf "($kver) is TOO OLD ($1 or later required)\n" "$kver"; 
+     printf "ERROR: Linux Kernel ($kver) is TOO OLD ($1 or later required)\n" "$kver"; 
      return 1; 
    fi
 }
 
 # Coreutils first because-sort needs Coreutils >= 7.0
-ver_check Coreutils      sort     7.0 || bail "coreutils"
+ver_check Coreutils      sort     7.0 || bail "--version-sort unsupported"
 ver_check Bash           bash     3.2
 ver_check Binutils       ld       2.13.1
 ver_check Bison          bison    2.7
@@ -57,9 +61,24 @@ ver_check Sed            sed      4.1.5
 ver_check Tar            tar      1.22
 ver_check Texinfo        texi2any 5.0
 ver_check Xz             xz       5.0.0
-ver_check git            git      2.25.0
-ver_check parted         parted   3.2
-# ver_kernel 4.14
+ver_kernel 4.14
 
+if mount | grep -q 'devpts on /dev/pts' && [ -e /dev/ptmx ]
+then echo "OK:    Linux Kernel supports UNIX 98 PTY";
+else echo "ERROR: Linux Kernel does NOT support UNIX 98 PTY"; fi
 
-bash pm.sh
+alias_check() {
+   if $1 --version 2>&1 | grep -qi $2
+   then printf "OK:    %-4s is $2\n" "$1";
+   else printf "ERROR: %-4s is NOT $2\n" "$1"; fi
+}
+echo "Aliases:"
+alias_check awk GNU
+alias_check yacc Bison
+alias_check sh Bash
+
+echo "Compiler check:"
+if printf "int main(){}" | g++ -x c++ -
+then echo "OK:    g++ works";
+else echo "ERROR: g++ does NOT work"; fi
+rm -f a.out
--- scripts/main.sh
+++ scripts/main.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-bash ./check.sh
+bail() { echo "FATAL: $1"; exit 1; }
+bash ./check.sh    || bail "unmet deps detected."
+sh part.sh
