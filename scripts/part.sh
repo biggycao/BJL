fabf44769b550a2753791d6483269302ce9ea397
biggycao <116410740+biggycao@users.noreply.github.com>
2024-01-03 14:38:24 +0800

    Update part.sh

--- scripts/part.sh
+++ scripts/part.sh
@@ -24,6 +24,18 @@ mkdir -v $LFS/sources
 chmod -v a+wt $LFS/sources
 wget https://mirrors.ustc.edu.cn/lfs/lfs-packages/lfs-packages-12.0.tar --continue --directory-prefix=$LFS/sources
 cd $LFS/sources
+wget 
+# Check if system is BIOS or UEFI
+if [ -d "/sys/firmware/efi" ]; then
+    echo "uefi"
+    wget https://ftp.gnu.org/gnu/grub/grub-2.06.tar.xz
+    wget https://www.linuxfromscratch.org/patches/blfs/12.0/grub-2.06-upstream_fixes-1.patch
+    wget https://unifoundry.com/pub/unifont/unifont-15.0.06/font-builds/unifont-15.0.06.pcf.gz
+    wget https://github.com/rhboot/efibootmgr/archive/18/efibootmgr-18.tar.gz
+else
+    echo "bios"
+fi
+
 tar -xf lfs-packages-12.0.tar
 cp 12.0/* .
 sh $LFS/BJLtempins/arch
